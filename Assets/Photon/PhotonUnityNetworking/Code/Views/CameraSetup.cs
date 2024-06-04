86ab8c9c196e1262586930ae22ad2cc1eeb6da65
Semin <tpals1127@naver.com>
2024-06-05 04:02:43 +0900

    resolve #1

---
+++ Assets/Photon/PhotonUnityNetworking/Code/Views/CameraSetup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+// using Cinemachine;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviour
+{
+    void Start()
+    {
+        // // 만약 자신이 로컬 플레이어라면
+        // if (PhotonView.IsMine)
+        // {
+        //     // 씬에 있는 시네머신 가상 카메라를 찾고
+        //     CinemachineVirtualCamera
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
