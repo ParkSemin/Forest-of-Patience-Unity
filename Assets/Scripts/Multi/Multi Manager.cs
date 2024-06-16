dce72fe639f95fff9b1b4addeb0ffa37572660b6
Semin <tpals1127@naver.com>
2024-06-17 04:30:46 +0900

    resolve #11

--- Assets/Scripts/Multi/Multi Manager.cs
+++ Assets/Scripts/Multi/Multi Manager.cs
@@ -10,16 +10,38 @@ public class MultiManager : MonoBehaviourPunCallbacks
 {
     private string gmaeVersion = "1.0";
     public TextMeshProUGUI connectionStateTxt;
-    public Button joinBtn;
-    public TMP_InputField mNickNameField;
+    public Button joinBtn, enterBtn;
+    public TMP_InputField mNickNameField, roomNumberField;
+    public GameObject roomNumberUI;
 
-    // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.GameVersion = gmaeVersion; // 접속에 필요한 정보(게임 버전) 설정
         PhotonNetwork.ConnectUsingSettings(); // 설정한 정보로 마스터 서버 접속 시도
         joinBtn.interactable = false; // 룸 접속 버튼을 잠시 비활성화
         connectionStateTxt.text = "Connecting to server..."; // 접속 시도 중임을 텍스트로 표시
+    }
+
+    // '게임 참가' 버튼을 누르면 방 번호를 입력하는 UI를 표시
+    public void OnInputRoomNumber() {
+        enterBtn.interactable = false;
+        roomNumberUI.SetActive(true);
+
+        // TMP_InputField의 이벤트에 콜백 함수 등록
+        roomNumberField.onValueChanged.AddListener(OnInputValueChanged);
+    }
+
+    // 값이 1~3자리 숫자일 때만 버튼 활성화
+    void OnInputValueChanged(string text)
+    {
+        int value;
+        bool isValid = int.TryParse(text, out value) && value >= 0 && value <= 999;
+        enterBtn.interactable = isValid && text.Length <= 3;
+    }
+
+    // 방 번호를 입력한 뒤 '완료' 버튼을 누르면 번호에 해당하는 방에 입장
+    public void OnEnterRoom() {
+        OnConnect(roomNumberField.text);
     }
 
     // 마스터 서버 접속 성공 시 자동 실행
@@ -38,7 +60,7 @@ public class MultiManager : MonoBehaviourPunCallbacks
     }
 
     // 룸 접속 시도
-    public void OnConnect() {
+    public void OnConnect(string roomNumber) {
         PhotonNetwork.NickName = mNickNameField.text;
 
         joinBtn.interactable = false; // 중복 접속 시도를 막기 위해 접속 버튼 잠시 비활성화
@@ -47,7 +69,7 @@ public class MultiManager : MonoBehaviourPunCallbacks
         {
             // 룸 접속 실행
             connectionStateTxt.text = "Entering the room..."; 
-            PhotonNetwork.JoinRandomRoom();
+            PhotonNetwork.JoinOrCreateRoom(roomNumber, new RoomOptions {MaxPlayers=8}, TypedLobby.Default, null);
         }
         else
         {
@@ -57,13 +79,13 @@ public class MultiManager : MonoBehaviourPunCallbacks
         }
     }
 
-    // (빈 방이 없어) 랜덤 룸 참가에 실패한 경우 자동 실행
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        connectionStateTxt.text = "Creating new Room...";
-        // 최대 4명 수용 가능한 빈 방 생성
-        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 4});
-    }
+    // // (빈 방이 없어) 랜덤 룸 참가에 실패한 경우 자동 실행
+    // public override void OnJoinRandomFailed(short returnCode, string message)
+    // {
+    //     connectionStateTxt.text = "Creating new Room...";
+    //     // 최대 4명 수용 가능한 빈 방 생성
+    //     PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 4});
+    // }
 
     // 룸에 참가 완료된 경우 자동 실행
     public override void OnJoinedRoom()
